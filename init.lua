10e4aa6fff15332bd134638c6c4eb208f198cb29
Linus Hollensberg <linus.hollensberg@gmail.com>
2026-03-02 23:26:36 +0100

    Added Oil file manager and telescope-grep-with-args

--- init.lua
+++ init.lua
@@ -99,6 +99,7 @@ require('lazy').setup({
   require 'lnh.plugins.harpoon',
   require 'lnh.plugins.undotree',
   require 'lnh.plugins.fugitive',
+  require 'lnh.plugins.oil',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
--- lua/lnh/plugins/fugitive.lua
+++ lua/lnh/plugins/fugitive.lua
@@ -1,12 +1,11 @@
 return {
-    'tpope/vim-fugitive',
-    config = function()
-      --
-      vim.keymap.set('n', '<leader>gs', vim.cmd.Git, { desc = 'git status' })
-      vim.keymap.set('n', '<leader>gf', ':Git fetch<CR>', { buffer = bufnr, desc = 'git fetch' })
-      vim.keymap.set('n', '<leader>gp', ':Git push<CR>', { desc = 'git [p]ush' })
-      vim.keymap.set('n', '<leader>gP', ':Git pull<CR>', { desc = 'git [P]ull' })
-      -- vim.keymap.set('n', '<leader>gP', vim.cmd.Git, { desc = 'git pull' })
-    end,
-  }
-
+  'tpope/vim-fugitive',
+  config = function()
+    --
+    vim.keymap.set('n', '<leader>gs', vim.cmd.Git, { desc = 'git status' })
+    vim.keymap.set('n', '<leader>gf', ':Git fetch<CR>', { buffer = bufnr, desc = 'git fetch' })
+    vim.keymap.set('n', '<leader>gp', ':Git push<CR>', { desc = 'git [p]ush' })
+    vim.keymap.set('n', '<leader>gP', ':Git pull<CR>', { desc = 'git [P]ull' })
+    -- vim.keymap.set('n', '<leader>gP', vim.cmd.Git, { desc = 'git pull' })
+  end,
+}
