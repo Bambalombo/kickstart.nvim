54a548d1f4c7f0e9ef3d101db193204dbd067cd0
Linus Hollensberg <linus.hollensberg@gmail.com>
2026-03-16 16:30:28 +0100

    Update toggleterm config

--- lua/lnh/plugins/toggleterm.lua
+++ lua/lnh/plugins/toggleterm.lua
@@ -1,5 +1,14 @@
 return {
   'akinsho/toggleterm.nvim',
   version = '*',
-  opts = {},
+  -- opts = {},
+  config = function()
+    require('toggleterm').setup {
+      size = 55,
+      open_mapping = [[<c-\>]],
+      shade_filetypes = {},
+      direction = 'float',
+    }
+    -- vim.keymap.set('n', ')
+  end,
 }
