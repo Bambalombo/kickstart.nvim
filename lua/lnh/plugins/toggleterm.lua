913d567f34699e37819637328512882c534c7b35
Linus Hollensberg <linus.hollensberg@gmail.com>
2026-05-05 16:25:00 +0200

    Update toggleterm settings

--- lua/lnh/plugins/toggleterm.lua
+++ lua/lnh/plugins/toggleterm.lua
@@ -4,10 +4,17 @@ return {
   -- opts = {},
   config = function()
     require('toggleterm').setup {
+      function(term)
+        if term.direction == 'horizontal' then
+          return 15
+        elseif term.direction == 'vertical' then
+          return vim.o.columns * 0.4
+        end
+      end,
       size = 55,
       open_mapping = [[<c-\>]],
       shade_filetypes = {},
-      direction = 'float',
+      direction = 'vertical',
     }
     vim.keymap.set('n', '<leader>ot', ':ToggleTerm<CR>', { desc = '[O]pen [T]erminal' })
   end,
